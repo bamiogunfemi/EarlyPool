e08f6a50923e17b8390eca063322562809ae48d7
bamiogunfemi <ayobamiarafat09@gmail.com>
2022-08-19 01:08:08 +0100

    feat: auth pages ui

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import DropDown from '../DropDown.js'
+import DropDown from "../DropDown";
 import "./index.css";
 import NavLogo from "../../assets/images/Group 7 1.svg";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -14,19 +14,32 @@ function NavBar() {
     <div>
       <div className="navBar">
         <div className="navBar__left">
-          <Link to='/'> <img src={NavLogo} alt="NavLogo" /></Link>
-          <Link to='/' className='hamburgerAlt left'> About </Link>
-          <Link to='/' className='hamburgerAlt left'> How it works </Link>
+          <Link to="/">
+            {" "}
+            <img src={NavLogo} alt="NavLogo" />
+          </Link>
+          <Link to="/" className="hamburgerAlt left">
+            {" "}
+            About{" "}
+          </Link>
+          <Link to="/" className="hamburgerAlt left">
+            {" "}
+            How it works{" "}
+          </Link>
         </div>
         <div className="navBar__right">
-          <Link to='/login' className='hamburgerAlt right'> Login </Link>
-          <Button className='hamburgerAlt bg-primary text-white normal-case'> <Link to='/signup'> Join Now </Link> </Button>
-          <MenuIcon className="hamburger"  onClick={() => setOpen(!open)} />
+          <Link to="/login" className="hamburgerAlt right">
+            {" "}
+            Login{" "}
+          </Link>
+          <Button className="hamburgerAlt bg-primary text-white normal-case">
+            {" "}
+            <Link to="/signup"> Join Now </Link>{" "}
+          </Button>
+          <MenuIcon className="hamburger" onClick={() => setOpen(!open)} />
         </div>
       </div>
-    <div  className="dropdown"> 
-      {open &&<DropDown/>}
-    </div>
+      <div className="dropdown">{open && <DropDown />}</div>
     </div>
   );
 }
